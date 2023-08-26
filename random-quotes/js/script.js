bff65df04fff390b64cfba1399eb458c77d81f7b
x0tic0p <xoticop@icloud.com>
2023-08-26 21:16:23 +0530

    quick-update

--- random-quotes/js/script.js
+++ random-quotes/js/script.js
@@ -1,19 +1,19 @@
+const apiUrl = 'https://api.quotable.io/random';
+
 const quoteElement = document.getElementById('quote');
 const newQuoteButton = document.getElementById('new-quote');
 
 async function fetchQuote() {
     try {
-        const response = await fetch('https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en');
+        const response = await fetch(apiUrl);
         const data = await response.json();
-        quoteElement.textContent = data.quoteText;
+        quoteElement.textContent = data.content;
     } catch (error) {
         console.error('Error fetching quote:', error);
-        quoteElement.textContent = 'An error occurred while fetching the quote.';
+        quoteElement.textContent = 'An error occurred while fetching the quote: ' + error.message;
     }
 }
 
-// Fetch a quote when the page loads
 fetchQuote();
 
-// Fetch a new quote when the button is clicked
 newQuoteButton.addEventListener('click', fetchQuote);
