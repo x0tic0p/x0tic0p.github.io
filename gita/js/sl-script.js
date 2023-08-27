09186ee3844b0160dfd730d588f91ecc413b7009
x0tic0p <xoticop@icloud.com>
2023-08-27 21:16:14 +0530

    major shloka section change

--- gita/js/sl-script.js
+++ gita/js/sl-script.js
@@ -1,50 +1,65 @@
-const chapterInput = document.getElementById('chapterInput');
-        const shlokaInput = document.getElementById('shlokaInput');
-        const submitButton = document.getElementById('submitButton');
-        const slokaFrame = document.getElementById('slokaFrame');
+document.addEventListener("DOMContentLoaded", function () {
+    const submitButton = document.getElementById("submitButton");
+    const slokaContainer = document.getElementById("slokaContainer");
+    const chapterShlokas = {
+        1: 47,
+        2: 72,
+        3: 43,
+        4: 42,
+        5: 29,
+        6: 47,
+        7: 30,
+        8: 28,
+        9: 34,
+        10: 42,
+        11: 55,
+        12: 20,
+        13: 35,
+        14: 27,
+        15: 20,
+        16: 24,
+        17: 28,
+        18: 78
+    };
 
-        const chapterShlokas = {
-            1: 47,
-            2: 72,
-            3: 43,
-            4: 42,
-            5: 29,
-            6: 47,
-            7: 30,
-            8: 28,
-            9: 34,
-            10: 42,
-            11: 55,
-            12: 20,
-            13: 35,
-            14: 27,
-            15: 20,
-            16: 24,
-            17: 28,
-            18: 78
-        };
-        
+    submitButton.addEventListener("click", function () {
+        const chapterInput = parseInt(document.getElementById("chapterInput").value);
+        const shlokaInput = parseInt(document.getElementById("shlokaInput").value);
 
-        submitButton.addEventListener('click', () => {
-            const chapter = chapterInput.value;
-            const shloka = shlokaInput.value;
+        if (chapterInput in chapterShlokas) {
+            const maxVerse = chapterShlokas[chapterInput];
             
-            if (!chapter || !shloka) {
-                alert('Please enter valid chapter and shloka numbers.');
-                return;
-            }
+            if (shlokaInput >= 1 && shlokaInput <= maxVerse) {
+                const apiUrl = `https://bhagavadgitaapi.in/slok/${chapterInput}/${shlokaInput}/`;
 
-            const chapterShlokaCount = chapterShlokas[chapter];
-            if (!chapterShlokaCount) {
-                alert('Invalid chapter number.');
-                return;
-            }
+                fetch(apiUrl)
+                    .then(response => response.json())
+                    .then(data => {
+                        const chapter = data.chapter;
+                        const verse = data.verse;
+                        const sloka = data.slok;
+                        const transliteration = data.transliteration;
+                        const author = data.tej.author;
+                        const ht = data.tej.ht;
 
-            if (shloka > chapterShlokaCount || shloka <= 0) {
-                alert(`Please enter a valid shloka number between 1 and ${chapterShlokaCount}.`);
-                return;
-            }
+                        const slokaHtml = `
+                            <p><strong>अध्याय ${chapter}, श्लोक ${verse}:</strong></p>
+                            <p><b>${sloka}</b></p>
+                            <p><b>Transliteration: ${transliteration}</b></p>
+                            <p><b>Hindi: ${ht}</b></p>
+                        `;
 
-            const url = `https://bhagavadgitaapi.in/slok/${chapter}/${shloka}/gita.svg`;
-            slokaFrame.src = url;
-        });
+                        slokaContainer.innerHTML = slokaHtml;
+                    })
+                    .catch(error => {
+                        console.error("Error fetching data:", error);
+                        slokaContainer.innerHTML = "<p>Error fetching sloka. Please try again.</p>";
+                    });
+            } else {
+                window.alert(`Invalid shloka number for Chapter ${chapterInput}. Please enter a shloka number between 1 and ${maxVerse}.`);
+            }
+        } else {
+            window.alert("Invalid chapter number. Please enter a valid chapter number.");
+        }
+    });
+});
